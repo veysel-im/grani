f801ed4491ec1a2951b635d50397e53273583be3
Veysel İNANÇ <vinanc90@gmail.com>
2016-10-06 23:08:50 +0300

    all update

--- script.js
+++ script.js
@@ -1,57 +1,20 @@
-﻿
-/// <reference path="ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" />
+﻿/// <reference path="ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" />
 
-//$(".kutu").append("<div class='boncuk'></div>");
-
-
-$("#btnEkle").click(function () {
-    konumBul();
-});
-
-function degerBul(e) {
-    var x = e.pageX;
-    var y = e.pageY;
-
-    $("#txtKonum").val(x + "-" + y);
-
-    //başla
-
-    //bitiş
-}
-
-function degerSil()
+function yemAt()
 {
-    $("#txtKonum").val("");
+
+    var yem = document.createElement("div");
+    yem1=$(yem);
+    yem1.addClass("yem");
+    yem1.css("top", Math.floor(Math.random() * window.screen.height));
+    yem1.css("left", Math.floor(Math.random() * window.screen.width));
+    yem1.css("background-color", "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ")");
+
+    
+    yem.style.position = "absolute";
+
+    $("#zemin").append(yem);
+
 }
 
-
-
-function konumBul()
-{
-    var kutuDiv = $(".kutu").offset();
-    
-    var anahomora = kutuDiv.left;
-    anahomora += 480;
-
-    var leviyosaa=kutuDiv.top;
-    leviyosaa += 480;
-
-    var degerx = Math.floor(Math.random() * anahomora);
-
-    while (degerx < anahomora - 480 || degerx > anahomora) {
-        degerx = Math.floor(Math.random() * anahomora);
-    }
-
-    var degery = Math.floor(Math.random() * leviyosaa);
-
-    while (degery < leviyosaa - 480 || degery > leviyosaa) {
-        degery = Math.floor(Math.random() * leviyosaa);
-    }
-    
-    
-    var renk = "rgb(" + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ");";
-
-    $(".kutu").append("<div class='boncuk' style='left:"+degerx+"px;top:"+degery+"px;background-color:"+renk+"'></div>");
-
-
-}
+setInterval(yemAt, 1);
